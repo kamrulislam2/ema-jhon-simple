c4be02f683bc72c666dd671710bf4a351e14879b
kamrulislam2 <kamrulislamABK@gmail.com>
2023-03-28 17:52:18 +0600

    order summary added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,19 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   let total = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    // product.quantity = product.quantity || 1;
+    total = total + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = (total * 7) / 100;
   const grandTotal = total + totalShipping + tax;
   return (
     <div className="cart">
       <h5>Order Summary</h5>
-      <p>Selected items: {cart.length}</p>
+      <p>Selected items: {quantity}</p>
       <p>Total Price: ${total.toFixed(2)}</p>
       <p>Total Shipping Charge: ${totalShipping.toFixed(2)}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
